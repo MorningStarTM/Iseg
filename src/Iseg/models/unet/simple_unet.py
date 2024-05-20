8b509be4bbb4272f92aa682ab97ce44a9dccc5b0
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-05-20 23:00:12 +0530

    unet architecture created

--- src/Iseg/models/unet/simple_unet.py
+++ src/Iseg/models/unet/simple_unet.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
+from tensorflow.keras.models import Model
 
 
 
 class Unet:
-    def __init__(self, num_filers:list, num_block:int, input_shape:tuple, n_class:int, activation:str, conv_block_size=1):
+    def __init__(self, num_filers:list, input_shape:tuple, n_class:int, activation:str, conv_block_size=1):
         self.num_filters = num_filers
-        self.num_block: num_block
+        self.num_block =  len(num_filers)
         self.input_shape = input_shape
         self.conv_block_size = conv_block_size
         self.n_class = n_class
         self.activation = activation
 
-    def conv_block(input, filters):
+    def conv_block(self, input, filters):
         x = layers.Conv2D(filters, 3, padding='same')(input)
         x = layers.BatchNormalization()(x)
         x = layers.Activation("relu")(x)
@@ -22,7 +23,7 @@ class Unet:
     
     def encoder_block(self, input, filters:int, num_blocks=1):
         x = input
-        for _ in range(num_blocks):
+        for i in range(num_blocks):
             x = self.conv_block(x, filters)
         p = layers.MaxPool2D((2, 2))(x)
         return x, p
@@ -31,7 +32,7 @@ class Unet:
     def decoder_block(self, input, skip_features, num_filters:int, num_blocks=1):
         x = layers.Conv2DTranspose(num_filters, (2,2), strides=2, padding='same')(input)
         x = layers.Concatenate()([x, skip_features])
-        for _ in range(num_blocks):
+        for i in range(num_blocks):
             x = self.conv_block(x, num_filters)
         return x
     
@@ -41,19 +42,18 @@ class Unet:
         skips = []
         x = inputs
 
-        for i in range(len(self.num_filters)):
-            x, p = self.encoder_block(x, self.num_filters[i], num_blocks=self.conv_block_size)
-            skips.append(x) 
+        for i in range(self.num_block):
+            s, x = self.encoder_block(x, filters=self.num_filters[i])
+            skips.append(s)
 
-        b = self.conv_block(p, self.num_filters[-1] * 2)
+        b = self.conv_block(x, self.num_filters[-1] * 2)
+        print(b.shape)
 
+        for i in range(self.num_block):
+            b = self.decoder_block(b, skips[-(i+1)], self.num_filters[-(i+1)])
 
-        skips = reversed(skips[:-1])  # Exclude the last skip connection from the encoder path
-        for i, skip in enumerate(skips):
-            b = self.decoder_block(b, skip, self.num_filters[-(i+2)], num_blocks=self.num_blocks)
+        outputs = layers.Conv2D(1, 1, padding='same', activation='sigmoid', name="classification_layer")(b)
 
-
-        outputs = layers.Conv2D(self.n_class, 1, padding='same', activation=self.activation)(b)
-
-        model = layers.Model(inputs, outputs, name='U-Net')
+        model = Model(inputs, outputs, name='U-Net')
         return model
+    
