1bf269ce1b4ee3234091bca556f4510c6338840a
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-05-24 18:56:01 +0530

    attention unet created

--- src/Iseg/models/unet/unet.py
+++ src/Iseg/models/unet/unet.py
@@ -4,6 +4,10 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.models import Model
 
 class Unet:
+    """
+    This class for U-Net architecture.
+    
+    """
     def __init__(self, num_filters: list, input_shape: tuple, n_class: int, activation: str, conv_block_size=1):
         self.num_filters = num_filters
         self.num_block = len(num_filters)
@@ -20,6 +24,18 @@ class Unet:
         return x
     
     def encoder_block(self, input, filters: int, num_blocks=1):
+        """
+        This function buid encoder for unet
+
+        Args:
+            input (array)
+            filter (int)
+            num_blocks (int)
+
+        Returns:
+            x (array) with same shape of input shape
+            p (array) with maxpooling applied shape
+        """
         x = input
         for i in range(num_blocks):
             x = self.conv_block(x, filters)
@@ -104,6 +120,109 @@ class Unet:
 
 
 
+class AttentionUNet(Model):
+    def __init__(self, input_shape, n_filters, n_class, activation, conv_block_size=1):
+        super(AttentionUNet, self).__init__()
+        self.num_filters = n_filters
+        self.num_block = len(n_filters)
+        self.input_shape = input_shape
+        self.conv_block_size = conv_block_size
+        self.n_class = n_class
+        self.activation = activation
 
 
+    def conv_block(self, input_layer, num_filters):
+        x = layers.Conv2D(num_filters, 3, padding='same')(input_layer)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+        
+        return x
+    
+    def attention_layer(self, g, s, num_filters):
+        """
+        This function make attention mechanism.
 
+        Args:
+            g (array) output of decoder
+            s (array) skip connection array
+        
+        Return:
+            out (array)
+        
+        """
+        wg = layers.Conv2D(num_filters, 1, padding='same')(g)
+        wg = layers.BatchNormalization()(wg)
+        
+        ws = layers.Conv2D(num_filters, 1, padding='same')(s)
+        ws = layers.BatchNormalization()(ws)
+        
+        out = layers.Activation('relu')(wg + ws)
+        out = layers.Conv2D(num_filters, 1, padding='same')(out)
+        out = layers.Activation('sigmoid')(out)
+        
+        return out 
+    
+
+    def encoder_block(self, input, filters: int, num_blocks=1):
+        """
+        This function buid encoder for unet
+
+        Args:
+            input (array)
+            filter (int)
+            num_blocks (int)
+
+        Returns:
+            x (array) with same shape of input shape
+            p (array) with maxpooling applied shape
+        """
+        x = input
+        for i in range(num_blocks):
+            x = self.conv_block(x, filters)
+        p = layers.MaxPool2D((2, 2))(x)
+        return x, p
+    
+    def decoder_block(self, input, skip_features, num_filters: int, num_blocks=1):
+        """
+        This function for build decoder block
+        
+        Args:
+            input (array)
+            skip feature (array)
+            num_filters (int)
+            num_blocks (int)
+
+        Returns:
+            x (array)
+        """
+        x = layers.UpSampling2D(interpolation='bilinear')(input)
+        s = self.attention_layer(x, skip_features, num_filters)
+        x = layers.Concatenate()([x, s])
+        for i in range(num_blocks):
+            x = self.conv_block(x, num_filters)
+        return x
+    
+
+    def build_att_unet(self):
+        """
+        This function will build the Attention U-Net architecture.
+        """
+        inputs = layers.Input(self.input_shape)
+        skips = []
+        x = inputs
+
+        for i in range(self.num_block):
+            s, x = self.encoder_block(x, filters=self.num_filters[i])
+            skips.append(s)
+
+        b = self.conv_block(x, self.num_filters[-1] * 2)
+        print(b.shape)
+
+        for i in range(self.num_block):
+            b = self.decoder_block(b, skips[-(i+1)], self.num_filters[-(i+1)])
+
+        outputs = layers.Conv2D(self.n_class, 1, padding='same', activation=self.activation, name="classification_layer")(b)
+
+        model = Model(inputs, outputs, name='Attention U-Net')
+        return model
+    
